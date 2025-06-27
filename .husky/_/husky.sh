f67eb13218ac5bd239b3263f77bafb038cfa3ea3
vinod120 <vinodgate2016@gmail.com>
2025-06-27 22:47:53 +0530

    changes

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,1 +1,9 @@
-404: Not Found
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
