d12bd5bfc9d7dfb5c6dd76e20c512c2bf2f09fc1
vinod120 <vinodgate2016@gmail.com>
2025-06-30 11:26:30 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
-const unusedVar = 42; // Unused variable
-console.log("checking... testing");createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
